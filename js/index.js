32fa3203811bcbfa082c58947ed69e1581376e7e
MaiNgocChau9 <chauaurora9@gmail.com>
2025-01-04 21:26:37 +0700

    Add user registration and login functionality, update user interface elements

--- js/index.js
+++ js/index.js
@@ -71,3 +71,17 @@ categoryCards.forEach(card => {
         card.style.transform = 'translateY(0)';
     });
 });
+
+const userNameSection = document.querySelector(".userNameSection");
+const login_register = document.querySelector(".login_register");
+const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+
+if (currentUser) {
+    userNameSection.textContent = currentUser.name;
+    userNameSection.classList.remove("d-none"); // Sử dụng class của Bootstrap
+    login_register.classList.add("d-none");     // Thay vì dùng style.display
+} else {
+    userNameSection.textContent = "";
+    userNameSection.classList.add("d-none");
+    login_register.classList.remove("d-none");
+}
